e61462858d877f34c11e516f35389b5cbd5cf4fa
KeisukeImai <keisuke@KIMac.local>
2016-11-27 21:42:41 +0900

    アプリ起動時に画像検索APIを叩く

--- app/src/main/java/com/app/kiima/clopro/fragment/TopFragment.java
+++ app/src/main/java/com/app/kiima/clopro/fragment/TopFragment.java
@@ -3,16 +3,27 @@ package com.app.kiima.clopro.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.app.kiima.clopro.R;
+import com.app.kiima.clopro.http.FlickrImageSearchService;
+import com.app.kiima.clopro.http.model.ImageSearch;
 import com.app.kiima.clopro.sample.SampleAdapter;
 import com.app.kiima.clopro.sample.SampleData;
 import com.etsy.android.grid.StaggeredGridView;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * トップ画面
@@ -59,5 +70,37 @@ public class TopFragment extends Fragment {
         }
 
         mGridView.setAdapter(mAdapter);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.flickr.com")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        FlickrImageSearchService service = retrofit.create(FlickrImageSearchService.class);
+
+        Map<String, String> query = new HashMap<>();
+        query.put("method", "flickr.photos.search");
+        query.put("api_key", "4ba3dd55a1862ea55f0fa7c5a8fdb616");
+        query.put("sort", "relevance");
+        query.put("privacy_filter", "1");
+        query.put("content_type", "1");
+        query.put("per_page", "1");
+        query.put("format", "json");
+        query.put("nojsoncallback", "1");
+        query.put("text", "cat");
+        Call<ImageSearch> response1 = service.search(query);
+
+        response1.enqueue(new Callback<ImageSearch>() {
+            @Override
+            public void onResponse(Call<ImageSearch> call, Response<ImageSearch> response) {
+                Log.d("test1", "success");
+                Log.d("test1", response.body().getPhotos().getPhoto().get(0).getId() + "");
+            }
+
+            @Override
+            public void onFailure(Call<ImageSearch> call, Throwable t) {
+                Log.d("test1", "faile");
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/app/kiima/clopro/http/model/Photos.java
@@ -0,0 +1,72 @@
+package com.app.kiima.clopro.http.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Photos model
+ *
+ * Created by Tre3 on 2016/11/27.
+ */
+
+public class Photos {
+
+    @SerializedName("page")
+    @Expose
+    private int page;
+    @SerializedName("pages")
+    @Expose
+    private int pages;
+    @SerializedName("perpage")
+    @Expose
+    private int perpage;
+    @SerializedName("total")
+    @Expose
+    private String total;
+    @SerializedName("photo")
+    @Expose
+    private List<Photo> photo = new ArrayList<>();
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public int getPerpage() {
+        return perpage;
+    }
+
+    public void setPerpage(int perpage) {
+        this.perpage = perpage;
+    }
+
+    public String getTotal() {
+        return total;
+    }
+
+    public void setTotal(String total) {
+        this.total = total;
+    }
+
+    public List<Photo> getPhoto() {
+        return photo;
+    }
+
+    public void setPhotos(List<Photo> photos) {
+        this.photo = photos;
+    }
+}
